aca9b546c807194781874b1af839cba4b271ea30
dq-qiji <nguyendaoducquan@gmail.com>
2022-05-24 21:57:06 +0700

    ASP.NET core Web API - Task 2.0: Using MongoDB - In Progress

---
+++ BooksApi/BooksApi/Models/BookstoreDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace BooksApi.Models
+{
+    public class BookstoreDatabaseSettings : IBookstoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IBookstoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
